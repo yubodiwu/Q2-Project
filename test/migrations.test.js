02cfa5dc78dbdd97f9e66549c922bd5e4bb17e41
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-11-14 11:17:22 -0800

    split test files into routes, migrations, and seeds

---
+++ test/migrations.test.js
@@ -0,0 +1,122 @@
+/* eslint-disable camelcase */
+// jshint esversion: 6
+// jshint devel: true
+// jshint node: true
+// jshint browser: true
+// jshint mocha: true
+
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const assert = require('chai').assert;
+const {
+    suite,
+    test
+} = require('mocha');
+const knex = require('../knex');
+
+suite('part1 migrations', () => {
+    before((done) => {
+        knex.migrate.latest()
+            .then(() => {
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+
+    after((done) => {
+        knex.migrate.rollback()
+            .then(() => {
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+
+    test('company columns', (done) => {
+        knex('books').columnInfo()
+            .then((actual) => {
+                const expected = {
+                    id: {
+                        type: 'integer',
+                        maxLength: null,
+                        nullable: false,
+                        defaultValue: 'nextval(\'companies_id_seq\'::regclass)'
+                    },
+
+                    name: {
+                        type: 'character varying',
+                        maxLength: 255,
+                        nullable: false,
+                        defaultValue: '\'\'::character varying'
+                    },
+
+                    email: {
+                        type: 'character varying',
+                        maxLength: 255,
+                        nullable: false,
+                        defaultValue: '\'\'::character varying'
+                    },
+
+                    industry: {
+                        type: 'character varying',
+                        maxLength: 255,
+                        nullable: false,
+                        defaultValue: '\'\'::character varying'
+                    },
+
+                    website: {
+                        type: 'text',
+                        maxLength: null,
+                        nullable: false,
+                        defaultValue: '\'\'::text'
+                    },
+
+                    status: {
+                        type: 'text',
+                        maxLength: null,
+                        nullable: false,
+                        defaultValue: '\'\'::text'
+                    },
+
+                    logo_url: {
+                        type: 'timestamp with time zone',
+                        maxLength: null,
+                        nullable: false,
+                        defaultValue: 'now()'
+                    },
+
+                    locations: {
+                        type: 'Array',
+                        maxLength: null,
+                        nullable: false,
+                        defaultValue: '[]'
+                    },
+
+                    updated_at: {
+                        type: 'timestamp with time zone',
+                        maxLength: null,
+                        nullable: false,
+                        defaultValue: 'now()'
+                    }
+                };
+
+                for (const column in expected) {
+                    assert.deepEqual(
+                        actual[column],
+                        expected[column],
+                        `Column ${column} is not the same`
+                    );
+                }
+
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+});
