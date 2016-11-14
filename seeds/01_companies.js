00792a92b3889246f5428de3a798e6267b40651b
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-11-14 14:47:22 -0800

    added seed template

---
+++ seeds/01_companies.js
@@ -0,0 +1,17 @@
+'use strict';
+// jshint esversion: 6
+// jshint devel: true
+// jshint node: true
+// jshint browser: true
+// jshint mocha: true
+
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex('users').del()
+        .then(function() {
+            return knex('users').insert([
+                {
+                    // add something
+                }
+            ]);
+};
---
+++ seeds/02_users.js
@@ -0,0 +1,28 @@
+'use strict';
+// jshint esversion: 6
+// jshint devel: true
+// jshint node: true
+// jshint browser: true
+// jshint mocha: true
+
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex('table_name').del()
+        .then(function() {
+            return Promise.all([
+                // Inserts seed entries
+                knex('table_name').insert({
+                    id: 1,
+                    colName: 'rowValue1'
+                }),
+                knex('table_name').insert({
+                    id: 2,
+                    colName: 'rowValue2'
+                }),
+                knex('table_name').insert({
+                    id: 3,
+                    colName: 'rowValue3'
+                })
+            ]);
+        });
+};
