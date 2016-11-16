d3f7d5427f8a1602bd5cf82d373c3003f877e319
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-11-16 12:23:17 -0800

    added route for connections

---
+++ routes/connections.js
@@ -0,0 +1,14 @@
+'use strict';
+// jshint esversion: 6
+// jshint devel: true
+// jshint node: true
+// jshint browser: true
+// jshint mocha: true
+
+const express = require('express');
+const knex = require('../db/knex');
+var router = express.Router();
+
+
+
+module.exports = router;
