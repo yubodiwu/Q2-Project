58562a739d2f8cfea31c4784c1aef241b496fbcd
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-11-15 18:53:20 -0800

    did some of post route

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.get(`/`, function(req, res) {
 });
 
 router.get(`/new`, function(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
 });
 
 router.get(`/:id`, function(req, res) {
@@ -28,12 +28,25 @@ router.get(`/:id`, function(req, res) {
         .where('id',req.params.id)
         .returning('*')
         .then(function(user) {
-            res.send(user);
+            // console.log(user);
+            res.send(user[0]);
         });
 });
 
-router.get(`/users/:id`, function(req, res) {
-    // this should be the show request
+router.post(`/`, function(req, res) {
+    console.log(`post route is hit`);
+    console.log(`\n\nreq.body is`);
+    console.log(req.body);
+    knex('users')
+        .insert(req.body)
+        .returning('*')
+        .then(function(user) {
+            console.log(user);
+            res.send(user[0]);
+        })
+        .catch(function(err) {
+            console.log('post error: ' + err);
+        })
 });
 
 module.exports = router;
